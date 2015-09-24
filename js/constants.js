4fc1a99e537e039a4caeb6b2d09402dc54783737
Matthew Williams <mwilliamsnov6@gmail.com>
2015-09-24 15:46:27 -0700

    7 new puzzles created

--- js/constants.js
+++ js/constants.js
@@ -96,7 +96,106 @@ app.constant("learningMode", [
      regEx: new RegExp("[A-Za-z0-9_]+$",""),
      id: 11,
      attempted: false, completed: false
+  },
+
+  {
+     title: "Show Me Your Digits", difficulty: 1, 
+     description: "<font color='#CC4B09'> \\d </font> selects the first match of any digit from 0 to 9. Select all three numbers below using <font color='#CC4B09'> \\d </font>  and a <font color='#CC4B09'> + </font> ",
+     stringToSearch: "You've got the wrong number, this is... 912",
+     regEx: new RegExp("\\d+",""),
+     id: 12,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "Dots Everywhere!", difficulty: 1, 
+     description: "the <font color='#CC4B09'> . </font> is used to denote a wildcard character. it will match a number, a letter, a space, you name it! Use <font color='#CC4B09'> go </font> plus the <font color='#CC4B09'> . </font> character to select all the words! Do you need a flag?",
+     stringToSearch: "going going gone!",
+     regEx: new RegExp("go...","g"),
+     id: 13,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "Be Selective", difficulty: 1, 
+     description: "Using brackets <font color='#CC4B09'> [ ] </font> will select a match of anything inside in any order. The letters OUTSIDE the brackets much match IN ORDER. Knowing this, match ONLY the first 3 words by using <font color='#CC4B09'> [ ] </font>. I won't mention <font color='#CC4B09'> g </font> flag anymore, you should know when you need it.",
+     stringToSearch: "vain cain gain rain pain",
+     regEx: new RegExp("[vcg]ain","g"),
+     id: 14,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "I Don't Want These!", difficulty: 1, 
+     description: "Putting a <font color='#CC4B09'> ^ </font> inside the brackets will negate the contents. This means that <font color='#CC4B09'> [^abc] </font> will NOT select <font color='#CC4B09'> a b </font> or <font color='#CC4B09'> c </font> . Using <font color='#CC4B09'> ^ </font> and <font color='#CC4B09'> [ ] </font>, select only the middle 3 words",
+     stringToSearch: "vain cain gain rain pain",
+     regEx: new RegExp("[^vp]ain","g"),
+     id: 15,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "Home, Home On The Range", difficulty: 1, 
+     description: "You've seen a little about the <font color='#CC4B09'> - </font> character. It returns a range. So <font color='#CC4B09'> [1-4] </font> returns 1 through 4. <font color='#CC4B09'> [a-c] </font> returns a b, or c. Also, <font color='#CC4B09'> [^a-c] </font> returns things that are not a b, or c. Use range to select lmnop",
+     stringToSearch: "abcdefghijklmnopqrstuvwxyz",
+     regEx: new RegExp("[l-p]","g"),
+     id: 16,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "It's Symbolic", difficulty: 1, 
+     description: "Where <font color='#CC4B09'> \\w </font> selects alphanumberic and underscore, <font color='#CC4B09'> \\W </font> selects the OPPOSITE --- which is symbols, spaces, etc. Use <font color='#CC4B09'> \\W </font> to select all symbols and spaces",
+     stringToSearch: "n3v3r G0 FULL r37@rD",
+     regEx: new RegExp("[\\W]","g"),
+     id: 17,
+     attempted: false, completed: false
+  },
+
+  {
+     title: "Repeat After Me...", difficulty: 1, 
+     description: "The <font color='#CC4B09'> { } </font> is used to denote repetion. <font color='#CC4B09'> w{2} </font> will match ww but not w. You can give it a range as well. <font color='#CC4B09'> w{1,2} </font> matches w and ww but not www. Match ONLY words with MORE THAN 1 z.",
+     stringToSearch: "wazzzzupppp? wazup? waaaaazzzzzuupp???",
+     regEx: new RegExp("z{2,5}","g"),
+     id: 18,
+     attempted: false, completed: false
   }
+
+  // {
+  //    title: "Mind Blown: Last Word!", difficulty: 1, 
+  //    description: "Now type <font color='#CC4B09'> [A-Za-z0-9_] </font>, add a <font color='#CC4B09'> + </font> and then add a <font color='#CC4B09'> $ </font>. Remember, a <font color='#CC4B09'> + </font> matches one or more instance of the preceding token, a <font color='#CC4B09'> $ </font> matches the end of the string.",
+  //    stringToSearch: "Come with me if you want to live",
+  //    regEx: new RegExp("[A-Za-z0-9_]+$",""),
+  //    id: 19,
+  //    attempted: false, completed: false
+  // },
+
+  // {
+  //    title: "Mind Blown: Last Word! ", difficulty: 1, 
+  //    description: "Now type <font color='#CC4B09'> [A-Za-z0-9_] </font>, add a <font color='#CC4B09'> + </font> and then add a <font color='#CC4B09'> $ </font>. Remember, a <font color='#CC4B09'> + </font> matches one or more instance of the preceding token, a <font color='#CC4B09'> $ </font> matches the end of the string.",
+  //    stringToSearch: "Come with me if you want to live",
+  //    regEx: new RegExp("[A-Za-z0-9_]+$",""),
+  //    id: 20,
+  //    attempted: false, completed: false
+  // },
+
+  // {
+  //    title: "Mind Blown: Last Word! ", difficulty: 1, 
+  //    description: "Now type <font color='#CC4B09'> [A-Za-z0-9_] </font>, add a <font color='#CC4B09'> + </font> and then add a <font color='#CC4B09'> $ </font>. Remember, a <font color='#CC4B09'> + </font> matches one or more instance of the preceding token, a <font color='#CC4B09'> $ </font> matches the end of the string.",
+  //    stringToSearch: "Come with me if you want to live",
+  //    regEx: new RegExp("[A-Za-z0-9_]+$",""),
+  //    id: 21,
+  //    attempted: false, completed: false
+  // },
+
+  // {
+  //    title: "Mind Blown: Last Word! ", difficulty: 1, 
+  //    description: "Now type <font color='#CC4B09'> [A-Za-z0-9_] </font>, add a <font color='#CC4B09'> + </font> and then add a <font color='#CC4B09'> $ </font>. Remember, a <font color='#CC4B09'> + </font> matches one or more instance of the preceding token, a <font color='#CC4B09'> $ </font> matches the end of the string.",
+  //    stringToSearch: "Come with me if you want to live",
+  //    regEx: new RegExp("[A-Za-z0-9_]+$",""),
+  //    id: 22,
+  //    attempted: false, completed: false
+  // }
 
 ]);
 // ####################################################################################################################################################################################################
